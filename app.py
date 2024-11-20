6b1b1c3ee88fe349978f59b695ce73a2af451785
Sehrish <daudsehrish@gmail.com>
2024-11-19 20:56:48 -0800

    Update app.py with filtered fruits and keys integration

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+import keys
 
 app = Flask(__name__)
+
 @app.route("/")
+
 def index():
     fruits = [
         {"name": "apples", "quantity": 3},
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
-    return render_template("index.html", fruits=fruits)
+
+    filtered_fruits = [
+        fruit for fruit in fruits if fruit["name"].startswith("o") and fruit["quantity"] < 3
+    ]
+
+    # Print the fake keys 
+    print(keys.MY_SECRET_API_KEY_1)
+    print(keys.MY_SECRET_API_KEY_2)
+
+    return render_template("index.html", fruits=filtered_fruits)
